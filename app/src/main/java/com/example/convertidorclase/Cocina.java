0512bb711c0141fea76f37ef430b6a0b5ed4a0c0
fteruel <55563317+fteruel@users.noreply.github.com>
2020-05-11 14:59:54 -0300

    agregado cocina

--- app/src/main/java/com/example/convertidorclase/Cocina.java
+++ app/src/main/java/com/example/convertidorclase/Cocina.java
@@ -3,6 +3,11 @@ package com.example.convertidorclase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cocina extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class Cocina extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocina);
+
+        ListView CocinaListaLV = findViewById(R.id.CocinaListaLV);
+
+        List<String> listaCocina = new ArrayList<String>();
+
+        listaCocina.add("azucar");
+        listaCocina.add("harina");
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(getApplicationContext(),
+                                                                R.layout.fila_cocina, R.id.tituloFila,
+                                                                listaCocina);
+        CocinaListaLV.setAdapter(adaptador);
+
+
     }
 }
